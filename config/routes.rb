631a48e5d2cc811cfc90503d02f6c4a6cd2cee14
quanghuy1891996 <huy.nguyen7.intern@ntq-solution.com>
2019-08-23 11:21:22 +0700

    Finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/contact'
 
-  resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'static_pages#home'
