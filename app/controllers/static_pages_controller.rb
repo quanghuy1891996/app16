6709416a3c693e3c230817e2a85b192541f7f684
quanghuy1891996 <huy.nguyen7.intern@ntq-solution.com>
2019-09-04 10:12:37 +0700

    Implement basic login

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
-class StaticPagesController < ApplicationController
+  class StaticPagesController < ApplicationController
   def home
   end
 
   def help
   end
-end
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190823030515) do
+ActiveRecord::Schema.define(version: 20190902155106) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 20190823030515) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
